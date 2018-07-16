de0d27d745b379847216ad3f18e34165fb95d6a7
Brian Choi <humblesticker@gmail.com>
2018-07-15 19:10:32 -0700

    refactor prob A

--- codeforces/Stairway.cpp
+++ codeforces/Stairway.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int main() {
-    int n, num, prev = 0, stair = 0; cin >> n;
-    vector<int> count;
-    for(int i=0; i<n; i++) {
-        cin >> num;
-        if(num == 1 && i > 0) { count.push_back(prev); stair++; }
-        prev = num;
-    }
-    count.push_back(prev); stair++; 
+    int n; cin >> n;
+    int cur, prev; cin >> prev;
+    vector<int> steps;
 
-    cout << stair << "\n";
-    for(int c : count) cout << c << " ";
-    cout << "\n";
+    for(int i=1; i<n; i++) {
+        cin >> cur; if(cur == 1) steps.push_back(prev); 
+        prev = cur;
+    }
+    steps.push_back(prev); 
+
+    cout << steps.size() << "\n";
+    for(int step : steps) cout << step << " ";
     return 0;
 }
