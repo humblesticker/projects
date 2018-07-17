4e58c756988d7c858296a6561893f1eb333de5f3
Brian Choi <humblesticker@gmail.com>
2018-07-16 21:32:28 -0700

    C refactor > use shift instead

--- codeforces/PowerOfTwo.cpp
+++ codeforces/PowerOfTwo.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
-#include <vector>
 #include <unordered_map>
 using namespace std;
 
-bool find(unordered_map<int, int> &mm, vector<int> &power, int val) {
-    for(const auto &p : power) { 
-        auto found = mm.find(p - val); 
-        if(found == mm.end() || (val == p - val && found->second < 2)) continue; 
+bool find(unordered_map<int, int> &mm, int val) {
+    for(int i=1; i<32; i++) {
+        int num = (1 << i) - val; 
+        auto found = mm.find(num); 
+        if(found == mm.end() || (num == val && found->second < 2)) continue; 
         return true;
     }
     return false;
 }
 
 int main() {
-    int n, d, count = 0; cin >> n; 
-    unordered_map<int, int> mm; vector<int> power(31);
+    int n, d, count = 0; cin >> n; unordered_map<int, int> mm; 
     for(int i=0; i<n; i++) { cin >> d; mm[d]++; }
-    for(int i=0, f=1; i<power.size(); i++, f*=2) power[i] = f;
 
     for(const auto &pair : mm)
-        if(!find(mm, power, pair.first)) count += pair.second;
+        if(!find(mm, pair.first)) count += pair.second;
 
     cout << count << "\n"; 
     return 0;
