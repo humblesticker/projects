49e226946cc706e285c8c463f261bb12bfdd681f
Brian Choi <humblesticker@gmail.com>
2018-07-16 21:26:22 -0700

    C refactor

--- codeforces/PowerOfTwo.cpp
+++ codeforces/PowerOfTwo.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <unordered_set>
 using namespace std;
 
-bool find(vector<int> &power, unordered_map<int, unordered_set<int>> &mm, int val, int pos) {
-    for(const auto &p : power) {
-        const auto &found = mm.find(p - val); if(found == mm.end()) continue;
-        for(const auto &s : found->second) if(s != pos) return true;
+bool find(unordered_map<int, int> &mm, vector<int> &power, int val) {
+    for(const auto &p : power) { 
+        auto found = mm.find(p - val); 
+        if(found == mm.end() || (val == p - val && found->second < 2)) continue; 
+        return true;
     }
     return false;
 }
 
 int main() {
-    int n, count = 0; cin >> n; 
-    unordered_map<int, unordered_set<int>> mm; vector<int> nums(n), power(31);
-    for(int i=0; i<nums.size(); i++) { cin >> nums[i]; mm[nums[i]].insert(i); }
+    int n, d, count = 0; cin >> n; 
+    unordered_map<int, int> mm; vector<int> power(31);
+    for(int i=0; i<n; i++) { cin >> d; mm[d]++; }
     for(int i=0, f=1; i<power.size(); i++, f*=2) power[i] = f;
 
-    for(int i=0; i<nums.size(); i++) if(!find(power, mm, nums[i], i)) count++; 
+    for(const auto &pair : mm)
+        if(!find(mm, power, pair.first)) count += pair.second;
+
     cout << count << "\n"; 
     return 0;
 }
