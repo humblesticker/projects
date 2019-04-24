17bd26fbe196d5f3cb2dc488ddda57daec3a42a8
Brian Choi <humblesticker@Brians-MacBook-Pro.local>
2019-04-23 23:22:23 -0700

    simpler

--- daily/smallestWindow.cpp
+++ daily/smallestWindow.cpp
@@ -1,63 +1,24 @@
-#include <tuple>
 #include <vector>
 #include <iostream>
 #include <limits>
+#include <algorithm>
 using namespace std;
 
-/*
-maxSoFar = min
-start = -1, end = -1
-iterate
-    maxSoFar = max(maxSoFar, cur)
+void range(vector<int>& nums) {
+    int maxSoFar = numeric_limits<int>::min(), start = -1, end = -1, N = nums.size();
     
-    if start < 0 
-        if cur > next start = i
-    else 
-        if cur < maxSoFar end = i
-
-    cout << start << end;
-*/
-class smallest {
-public:
-    int start(vector<int>& nums) {
-        vector<int> minSoFar(nums.size());
-        int min = numeric_limits<int>::max();
-
-        for(int i=nums.size()-1; i>=0; i--) {
-            if(nums[i] < min) min = nums[i];
-            minSoFar[i] = min;
-        }
-
-        for(int i=0; i<nums.size(); i++)
-            if(nums[i] > minSoFar[i]) return i;
-        return -1;
+    for(int i=0; i<N; i++) {
+        maxSoFar = max(maxSoFar, nums[i]);
+        if(start < 0) { if(i+1 < N && nums[i] > nums[i+1]) start = i; }
+        else { if(nums[i] < maxSoFar) end = i; }
     }
 
-    int end(vector<int>& nums) {
-        vector<int> maxSoFar(nums.size());
-        int max = numeric_limits<int>::min();
-
-        for(int i=0; i<nums.size(); i++) {
-            if(nums[i] > max) max = nums[i];
-            maxSoFar[i] = max;
-        }
-
-        for(int i=nums.size()-1; i>=0; i--) 
-            if(nums[i] < maxSoFar[i]) return i;
-        return -1;
-    }
-
-    tuple<int, int> getWindow(vector<int>& nums) {
-        return make_tuple(start(nums), end(nums));
-    } 
-};
+    cout << start << "," << end << endl;
+}
 
 int main() {
     int n; cin >> n; vector<int> nums(n);
     for(int i=0; i<n; i++) cin >> nums[i];
-
-    smallest s;
-    auto range = s.getWindow(nums);
-    cout << get<0>(range) << "," << get<1>(range) << endl;
+    range(nums);
     return 0;
 }
