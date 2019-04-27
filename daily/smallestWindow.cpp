c77909e408fc4caae33816519666f4f72a67f97f
Brian Choi <humblesticker@Brians-MacBook-Pro.local>
2019-04-26 23:27:27 -0700

    daily coding #2

--- daily/smallestWindow.cpp
+++ daily/smallestWindow.cpp
@@ -4,13 +4,35 @@
 #include <algorithm>
 using namespace std;
 
+/*
+left
+    from end 
+        track min
+        if cur > min left = i
+    return i
+
+right
+    from start 
+        track max
+        if cur < max right = i
+    return i
+
+test cases
+3 7 5 6 9
+3 4 5 2 9
+
+*/
 void range(vector<int>& nums) {
-    int maxSoFar = numeric_limits<int>::min(), start = -1, end = -1, N = nums.size();
+    int maxSoFar = numeric_limits<int>::min(), minSoFar = numeric_limits<int>::max(), start = -1, end = -1, N = nums.size();
     
+    for(int i=N-1; i>=0; i--) {
+        minSoFar = min(minSoFar, nums[i]);
+        if(nums[i] > minSoFar) start = i;
+    }
+
     for(int i=0; i<N; i++) {
         maxSoFar = max(maxSoFar, nums[i]);
-        if(start < 0) { if(i+1 < N && nums[i] > nums[i+1]) start = i; }
-        else { if(nums[i] < maxSoFar) end = i; }
+        if(nums[i] < maxSoFar) end = i;
     }
 
     cout << start << "," << end << endl;
